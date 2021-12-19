9340bca407f3e608968f0da67b87654fd0f8df62
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-19 12:02:55 +0300

    Нашли функцию от а - возвели в куб

---
+++ TaskTest_6/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления значения функции y = f(a)
+
+Console.Write("Введите число a -> ");
+
+double valueDouble = Convert.ToDouble(Console.ReadLine ());
+
+double resultY = valueDouble * valueDouble * valueDouble;
+
+Console.WriteLine(resultY);
