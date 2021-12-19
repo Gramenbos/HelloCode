18535f5171f928fb9f09ed04b4daf3835bdbda66
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-19 11:47:38 +0300

    Нашли максимальное из трех

---
+++ TaskTest_5/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти максимальное из трех чисел
+
+Console.Write("Введите первое число -> ");
+
+int valueInt1 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите второе число -> ");
+
+int valueInt2 = int.Parse(Console.ReadLine() ?? "0");
+
+Console.Write("Введите третье число -> ");
+
+int valueInt3 = int.Parse(Console.ReadLine() ?? "0");
+
+int [] Numbers = {valueInt1, valueInt2, valueInt3};
+
+int result = Numbers[0];
+
+for (int i = 0; i < Numbers.Length; i++)
+{
+    if (result < Numbers[i])
+    {
+        result = Numbers[i];
+    }
+}
+
+Console.Write("Максимальное число - " + result);
