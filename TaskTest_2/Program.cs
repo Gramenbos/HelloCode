6c339faa3c0a9734eb5dbae33772fb12583cb001
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-19 10:46:26 +0300

    Квадрат числа

---
+++ TaskTest_2/Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.Write("Введите первое число -> ");
+
+string valueString1 = Console.ReadLine();
+
+int valueInt1 = int.Parse(valueString1);
+
+Console.Write("Введите второе число -> ");
+
+string valueString2 = Console.ReadLine();
+
+int valueInt2 = int.Parse(valueString2);
+
+int square = valueInt2 * valueInt2;
+
+if (valueInt1 == square)
+{
+    Console.Write("Первое число является квадратом второго!");
+}
+else
+{
+    Console.Write("Ты не угадал! Первое число не является квадратом второго!");
+}
+
