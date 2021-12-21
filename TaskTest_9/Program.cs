eb41f740807af55b6aab2c8448c24ffb8171f78f
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-21 22:51:50 +0300

    Сделал задани: Показать четные числа от 1 до N

---
+++ TaskTest_9/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать четные числа от 1 до N
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int number = 2;
+
+while (number <= N)
+{
+    Console.Write(number + " ");
+    number = number + 2;
+}
