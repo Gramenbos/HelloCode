c5924c236e89577cf2b1140c384fc528aaae47d9
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-21 23:00:31 +0300

    Сделал задание с последней цифрой трехзначного числа

---
+++ TaskTest_10/Program.cs
@@ -0,0 +1,6 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Последняя цифра: " + (number % 100) % 10);
