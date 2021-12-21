3bf5fcb9d1a90727c9bbe26519d8eaf8c3cdb7d0
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-21 22:40:08 +0300

    Сделал задание вывести числа от -N до N

---
+++ TaskTest_8/Program.cs
@@ -0,0 +1,12 @@
+﻿// Показать числа от -N до N
+
+Console.Write("Введите N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int number = -N;
+
+while (number <= N)
+{
+    Console.WriteLine(number);
+    number++;
+}
