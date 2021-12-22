b38b4c82753006c01f896755d6bc90ff293410c7
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-22 20:56:54 +0300

    Добавил алгоритм проверки кратности

---
+++ TaskTest_14/Program.cs
@@ -0,0 +1,17 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+int number = 100;
+
+Console.WriteLine("Исходное число: " + number);
+Console.Write("Введите число для проверки кратности: ");
+
+int MyNumber = Convert.ToInt32(Console.ReadLine());
+
+if (number % MyNumber == 0)
+{
+    Console.WriteLine(number + " кратно Вашему числу!");
+}
+else
+{
+    Console.WriteLine("Числа не кратные, остаток от деления: " + number % MyNumber);
+}
