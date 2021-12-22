78342b17ba14b81ddba646299a782689ef097933
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-22 21:58:12 +0300

    Сделал задание 15 - проверка кратности 7 и 23

---
+++ JuniorTask_15/Program.cs
@@ -0,0 +1,12 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+
+Console.WriteLine("Введите число для проверки его кратности 7 и 23: ");
+
+int Number = Convert.ToInt32(Console.ReadLine());
+
+if (Number % 7 == 0 & Number % 23 == 0){
+    Console.WriteLine("Число кратно 7 и 23.");
+}
+else{
+    Console.WriteLine("Число не кратно 7 и 23.");
+}
