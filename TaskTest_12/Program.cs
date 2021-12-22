da4411d60badc06bc0527ac0ee7d6021c61e7094
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-22 20:37:56 +0300

    Добавил алгоритм нахождения наибольшей цифры двузначного числа

---
+++ TaskTest_12/Program.cs
@@ -0,0 +1,18 @@
+﻿// Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int number = new Random().Next(10, 100);
+
+Console.WriteLine("Случайное число: " + number);
+
+int FirstNumber = number / 10;
+int SecondNumber = number % 10;
+
+if (FirstNumber >= SecondNumber) 
+{
+    Console.WriteLine("Наибольшая цифра числа: "+ FirstNumber);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра числа: "+ SecondNumber);
+}
+
