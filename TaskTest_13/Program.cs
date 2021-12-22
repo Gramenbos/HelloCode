82b0ccf79c9de61cf666ac0c6c7e8218f366d259
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-22 20:45:03 +0300

    Добавил алгоритм удаления второй цифры трехзначного числа

---
+++ TaskTest_13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Удалить вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NewNumber = (number / 100) * 10 + (number % 10);
+
+Console.Write("Новое число: " + NewNumber);
