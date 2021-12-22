9c5512262e49849939cca3ec4a329df527149b6a
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-22 20:28:30 +0300

    Добавил программу для нахождения второй цифры трехзначного числа

---
+++ TaskTest_11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Secondumber = ((number % 100) - ((number % 100) % 10)) / 10;
+
+Console.Write("Вторая цифра: " + Secondumber);
