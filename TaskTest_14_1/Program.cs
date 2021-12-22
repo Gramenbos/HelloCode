7689cd3bd061c9585938fa308ed7e053fc2fdb6a
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-22 21:33:29 +0300

    Завершил блок Почувствуй себя Интерном (задания 0-14)

---
+++ TaskTest_14_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+Console.WriteLine("Введите любое целое число: ");
+string? Number = Console.ReadLine();                // Знак ? нужен, чтобы не было ошибки NULL
+int Lenght = Number.Length;
+
+if (Lenght > 2){
+    Console.WriteLine("Третья цифра: " + Number[2]);        // Number[2] - это второй символ в строке Number
+}
+else{
+    Console.WriteLine("Третьей цифры в Вашем числе нет!");
+}
