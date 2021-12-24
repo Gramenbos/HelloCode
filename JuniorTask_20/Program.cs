ca7bc22778c9558df11aac3b37f6ec528655661e
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-24 21:10:57 +0300

    Добавил программу для показа диапазона координат по номеру четверти плоскости

---
+++ JuniorTask_20/Program.cs
@@ -0,0 +1,15 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+
+int EnterInt(string Text)
+{                                                       // Эта функция печатает в консоли нужный текст и возвращает 
+    Console.WriteLine(Text);                            // введенное пользователем число
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+
+int Quoter = EnterInt("Введите номер четверти плоскости: ");
+
+if (Quoter == 1)    {Console.WriteLine("X > 0, Y > 0");}
+    else if (Quoter == 2)   {Console.WriteLine("X < 0, Y > 0");}
+    else if (Quoter == 3)   {Console.WriteLine("X < 0, Y < 0");}
+    else if (Quoter == 4)   {Console.WriteLine("X > 0, Y < 0");}
