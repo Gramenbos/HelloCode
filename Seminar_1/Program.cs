6f16bf529a52276b2ef42defc193cb1b5f306379
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-25 12:43:23 +0300

    Добавил код на семинаре 25 декабря

---
+++ Seminar_1/Program.cs
@@ -0,0 +1,23 @@
+﻿
+Console.WriteLine("Введите любое целое число: ");
+string? StringNumber = Console.ReadLine();
+
+try
+{
+    Convert.ToInt32(StringNumber);
+    string Temp = StringNumber ?? "";
+    int Length = Temp.Length;
+    if (Length > 2)
+    {
+        Console.WriteLine("Третья цифра: " + Temp[Length-3]);
+    }
+    else
+    {
+        Console.WriteLine("Третьей цифры в числе нет!");
+    }
+
+}
+catch
+{
+    Console.WriteLine("Неверный ввод!");
+}
