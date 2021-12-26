bca21c4a4500366591595b87127c01d23ae9a52b
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-26 21:14:10 +0300

    Сделал алгоритм для проверки любого числа на палиндром. Возникает ошибка overflow при введении больших чисел

--- JuniorTask_21/Program.cs
+++ JuniorTask_21/Program.cs
@@ -1,22 +1,63 @@
-﻿// 21. Программа проверяет, является ли пятизначное число палиндромом.
+﻿// 21. Программа проверяет, является ли ЛЮБОЕ число палиндромом.
 
-Console.WriteLine("Введите пятизначное число: ");
-int Number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число: ");
+string? StringNumber = Console.ReadLine();
+if (StringNumber == null) StringNumber = String.Empty; //Если значение NULL, то присвоить пустую строку 
+int Number = int.Parse(StringNumber);
+int NumberLength = StringNumber.Length;
+Console.WriteLine(Number);
 
-// Инициализируем массив и наполняем массив цифрами из введенного числа
-int[] Array = new int[5];
-for (int i = 0, j = 10000; i < 5; i++, j = j / 10)
+/*
+int[] CreateArrayFromConsole() // Метод для преобразования введенного числа в массив
 {
-    Array[i] = Number / j;
-    Number = Number - Array[i] * j;
+    try
+    {
+        Console.WriteLine("Введите число: ");
+        string? StringNumber = Console.ReadLine();
+        if (StringNumber == null) StringNumber = String.Empty; //Если значение NULL, то присвоить пустую строку 
+        int Number = int.Parse(StringNumber);
+        int NumberLength = StringNumber.Length;
+        int NumberFactor = 1;
+        for (int i = 1; i < NumberLength; i++)
+        {
+            NumberFactor *= 10;             // Получаю 10 в степени X, где X - количество знаков в числе
+        }
+
+        // Инициализируем массив и наполняем массив цифрами из введенного числа
+        int[] Array = new int[NumberLength];
+        for (int i = 0, j = NumberFactor; i < NumberLength; i++, j = j / 10)
+        {
+            if (j == 0) break;
+            Array[i] = Number / j;
+            Number = Number % j;
+        }
+        return Array;
+    }
+    catch
+    {
+        Console.WriteLine("Input error.");
+        int[] Array = { 0 };
+        return Array;
+    }
+
 }
 
 //Проверяем массив на палиндром
-if (Array[0] == Array[4] & Array[1] == Array[3])
+int[] Array = CreateArrayFromConsole();
+int j = Array.Length - 1;
+for (int i = 0; i < Array.Length / 2; i++, j--)
 {
-    Console.WriteLine("Ваше число является палиндромом!");
+    if (Array[i] != Array[j])
+    {
+        Console.WriteLine("Ваше число - НЕ палиндром.");
+        break;
+    }
+    else
+    {
+        if (i >= j - 2)
+        {
+            Console.WriteLine("Ваше число является палиндромом!");
+        }
+    }
 }
-else
-{
-    Console.WriteLine("Ваше число - НЕ палиндром.");
-}
+/*
