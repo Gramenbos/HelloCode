731b1158a5caaed8144c419cb6e28597413d0d67
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-26 21:28:41 +0300

    Сделал алгоритм для проверки любого числа на палиндром. Работает, в том числе проверяет ошибки

--- JuniorTask_21/Program.cs
+++ JuniorTask_21/Program.cs
@@ -1,13 +1,5 @@
 ﻿// 21. Программа проверяет, является ли ЛЮБОЕ число палиндромом.
 
-Console.WriteLine("Введите число: ");
-string? StringNumber = Console.ReadLine();
-if (StringNumber == null) StringNumber = String.Empty; //Если значение NULL, то присвоить пустую строку 
-int Number = int.Parse(StringNumber);
-int NumberLength = StringNumber.Length;
-Console.WriteLine(Number);
-
-/*
 int[] CreateArrayFromConsole() // Метод для преобразования введенного числа в массив
 {
     try
@@ -44,20 +36,22 @@ int[] CreateArrayFromConsole() // Метод для преобразования
 
 //Проверяем массив на палиндром
 int[] Array = CreateArrayFromConsole();
-int j = Array.Length - 1;
-for (int i = 0; i < Array.Length / 2; i++, j--)
+int j = Array.Length - 1;                       //Индекс последнего элемента в массиве
+for (int i = 0; i < Array.Length / 2; i++, j--) //Проверяем первую половину массива на совпадение 
+                                                //со второй половиной (с конца)
 {
-    if (Array[i] != Array[j])
+    if (Array[i] != Array[j]) //При первом несовпадении зеркальных элементов выдает сообщение и завершает цикл
     {
         Console.WriteLine("Ваше число - НЕ палиндром.");
         break;
     }
     else
     {
-        if (i >= j - 2)
+        if (i >= j - 2)     //Выводит сообщение об успешной проверке только после того, 
+                            //как разница между проверяемыми элементами будет меньше 2 
+                            //(то есть соседние числа или через одно)
         {
             Console.WriteLine("Ваше число является палиндромом!");
         }
     }
 }
-/*
