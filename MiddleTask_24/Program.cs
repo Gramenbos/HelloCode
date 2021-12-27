07fd0c6bb2f8a3c39b0e805345a674a06ddbc77a
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-27 22:03:06 +0300

    Сделал задание 24 - таблицу кубов чисел

---
+++ MiddleTask_24/Program.cs
@@ -0,0 +1,17 @@
+﻿// 24. Найти кубы чисел от 1 до N
+
+int EnterInt(string Text) // Эта функция печатает в консоли нужный текст и возвращает введенное пользователем число
+{                                                    
+    Console.Write(Text);                          
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+
+int N = EnterInt("Введите N: ");
+
+Console.WriteLine("Число   |     Куб числа");
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine("{0,5}   |{1,10}", i, i * i * i);
+}
