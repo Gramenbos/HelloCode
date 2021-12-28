214a98555b2819da16c81f949a2116c651350c3c
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-28 20:32:08 +0300

    Сделал задание 25 - найти сумму чисел от 1 до А

---
+++ MiddleTask_25/Program.cs
@@ -0,0 +1,17 @@
+﻿// 25. Найти сумму чисел от 1 до А
+
+int EnterInt(string Text) // Эта функция печатает в консоли нужный текст и возвращает введенное пользователем число
+{                                                    
+    Console.Write(Text);                          
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+
+int A = EnterInt("Введите A: ");
+int Sum = 0;
+for (int i = 1; i <= A; i++)
+{
+    Sum += i;
+}
+
+Console.WriteLine("Сумма чисел от 1 до А равна " + Sum);
