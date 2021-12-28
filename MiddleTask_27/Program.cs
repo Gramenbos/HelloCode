cc14af5f600c1d403ef8ea6b984188d6e576001f
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-28 21:39:33 +0300

    Сделал задание 27 - определить количество цифр в числе

---
+++ MiddleTask_27/Program.cs
@@ -0,0 +1,17 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.WriteLine("Введите число: ");
+string? StringNumber = Console.ReadLine();
+if (StringNumber == null) StringNumber = String.Empty; //Если значение NULL, то присвоить пустую строку 
+try
+{
+    ulong Number = Convert.ToUInt64(StringNumber);
+    int NumberLength = StringNumber.Length;
+    Console.WriteLine($"В вашем числе {NumberLength} цифр.");
+}
+catch (System.Exception)
+{
+    Console.WriteLine("Input error");
+     Environment.Exit(1);
+}
+
