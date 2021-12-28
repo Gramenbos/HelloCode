0642d045712de011b069f5e99e9762bd78899494
Evgeniy Murzanaev <gramenbos@gmail.com>
2021-12-28 20:45:02 +0300

    Сделал задание 26 - возвести А в степень В

---
+++ MiddleTask_26/Program.cs
@@ -0,0 +1,19 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+int EnterInt(string Text) // Эта функция печатает в консоли нужный текст и возвращает введенное пользователем число
+{                                                    
+    Console.Write(Text);                          
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+
+int A = EnterInt("Введите A: ");
+int B = EnterInt("В какую степень возвести число А? ");
+
+int Degree = 1;
+for (int i = 1; i <= B; i++)
+{
+    Degree *= A;
+}
+
+Console.WriteLine($"Число {A} в {B} степени равно {Degree}");
