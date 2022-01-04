8de85ca0e8283d76912b3c5a62f7f546ee8f3ca2
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-01-04 10:08:39 +0300

    Сделал задание 29 - вычисление произведения чисел от 1 до N

---
+++ MiddleTask_29/Program.cs
@@ -0,0 +1,17 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N
+
+int EnterInt(string Text) // Эта функция печатает в консоли нужный текст и возвращает введенное пользователем число
+{                                                    
+    Console.Write(Text);                          
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
+}
+
+int N = EnterInt("Введите N: ");
+int Multyplication = 1;
+for (int i = 1; i <= N; i++)
+{
+    Multyplication *= i;
+}
+
+Console.WriteLine($"Произведение чисел от 1 до {N} равно {Multyplication}");
