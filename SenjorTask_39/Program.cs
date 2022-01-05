45487679796f748ae809a5c58a434eb65adfb92d
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-01-05 11:13:30 +0300

    Сделал задание 39 - найти произведение пар чисел в одномерном массиве, где парой считается первый и последний элемент, второй и предпоследний, и так далее

---
+++ SenjorTask_39/Program.cs
@@ -0,0 +1,22 @@
+﻿// 39. Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+void FillArray(int[] Array, int MinNumber, int MaxNumber) //Метод для заполнения массива случайными числами (печать - для проверки)
+{
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(MinNumber, MaxNumber + 1); //Диапазон случайных чисел для заполнения массива
+        Console.Write($"{Array[i]} ");
+    }
+}
+
+int[] Array = new int[10];     //Создал массив, состоящий из 10 элементов
+FillArray(Array, 0, 9);       //Вызвал метод для заполнения массива с указанием диапазона чисел
+Console.WriteLine();
+
+int Multiplication;    //Произведение пар чисел в массиве
+
+for (int i = 0, j = Array.Length - 1; i < Array.Length / 2; i++, j--)    //i - индексы с начала массива, j - с конца. Проверяем до середины массива
+{
+    Multiplication = Array[i] * Array[j];
+    Console.WriteLine("Произведение {0}-й пары чисел равно {1}", i + 1, Multiplication);
+}
