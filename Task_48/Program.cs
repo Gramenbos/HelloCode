2d4b1bce9307fc12f43ec04821cd880365bed26d
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-01-15 22:53:53 +0300

    Сделал задание 48 - показать двумерный массив размером m*n, заполненный целыми числами

---
+++ Task_48/Program.cs
@@ -0,0 +1,41 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+int EnterInt(string text) // Эта функция печатает в консоли нужный текст и возвращает введенное пользователем число
+{
+    Console.Write(text);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+void FillTwoDimentionalArray(int[,] array, int minNumber, int maxNumber)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = rnd.Next(minNumber, maxNumber + 1);
+        }
+    }
+}
+
+void PrintTwoDimentionalArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Вам предлагается определить размер массива.");
+int arrayRows = EnterInt("Введите количество строк: ");
+int arrayColumns = EnterInt("Введите количество столбцов: ");
+
+int[,] workArray = new int[arrayRows, arrayColumns];
+FillTwoDimentionalArray(workArray, 0, 9);
+PrintTwoDimentionalArray(workArray);
