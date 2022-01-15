e15385a50fad1a670fb5c3d57dc9ae587a5e8c7a
Evgeniy Murzanaev <gramenbos@gmail.com>
2022-01-15 11:40:10 +0300

    Поработал на семинаре 15.01.2022

--- Seminar_1/Program.cs
+++ Seminar_1/Program.cs
@@ -1,29 +1,33 @@
-﻿//40. В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+﻿// 44. Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+// Если k1 = k2, то прямые параллельны!!! Если k различны, то прямые будут пересекаться
+// Если b1 = b2, то прямые пересекаются в точке (0;b)
+// Алгоритм решения такой: в одно из уравнений вставляем вместо "y" вставляем второе уравнение, и находим "x"
+// Потом с помощью любого из уравнений находим "y"
 
-void FillAndPrintArray(int[] array, int Min, int Max)
+int EnterInt(string Text) // Эта функция печатает в консоли нужный текст и возвращает введенное пользователем число
 {
-    Random rnd = new Random();
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = rnd.Next(Min, Max + 1);
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
+    Console.Write(Text);
+    int Number = Convert.ToInt32(Console.ReadLine());
+    return Number;
 }
 
-(int, int) MinMaxArray(int[] array)
-{
-    int minNumber = array[0];
-    int maxNumber = array[0];
-    foreach (int number in array)
-    {
-        if (number < minNumber) minNumber = number;
-        if (number > maxNumber) maxNumber = number;
-    }
-    return (minNumber, maxNumber);
-}
+Console.Clear();
+Console.WriteLine("Вам предлагается ввести значение переменных для уравнений: ");
+Console.WriteLine("y = k1 * x + b1,  y = k2 * x + b2");
 
-int[] Array = new int[12];
-FillAndPrintArray(Array, 0, 9);
-(int min, int max) = MinMaxArray(Array);
-Console.WriteLine($"Разница между максимальным и минимальным элементами массива равна {max - min}");
+double k1 = EnterInt("Введите k1: ");
+double k2 = EnterInt("Введите k2: ");
+double b1 = EnterInt("Введите b1: ");
+double b2 = EnterInt("Введите b2: ");
+
+if (k1 != k2)
+{
+    double x = (b2 - b1) / (k1 - k2);
+    double y = k1 * x + b1;
+    Console.WriteLine("Координаты точки пересечения прямых: x = {0}, y = {1}", x, y);
+}
+else
+{
+    if (b1 == b2) Console.WriteLine("Прямые совпадают!");
+    else Console.WriteLine("Прямые паралельны!");
+}
